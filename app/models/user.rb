4195890ec728d8e118324e4e147758e6def0ace6
sho-kasama <sho-kasama@trunk.fm>
2019-04-21 18:38:42 +0900

    ref #7 add: controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
 
+  mount_uploader :image, ImageUploader
+
   has_many :microposts
   validates :name, presence: true    # 「FILL_IN」をコードに置き換えてください
   validates :email, presence: true    # 「FILL_IN」をコードに置き換えてください
