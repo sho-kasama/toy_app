396932db18cc7b2b542d1a4f1b8df17ba0dec71e
sho-kasama <sho-kasama@trunk.fm>
2019-04-21 18:36:51 +0900

    ref #7 add: schema file & image columns to Users model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190419063006) do
+ActiveRecord::Schema.define(version: 20190421090921) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20190419063006) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
 end
